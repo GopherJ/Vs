dbbff485e58812616c6c610e72201936e53148a6
Cheng JIANG <cheng.jiang@outlook.fr>
2018-06-04 12:40:42 +0200

    fix bug of d3Slider

--- src/components/d3Slider/d3Slider.vue
+++ src/components/d3Slider/d3Slider.vue
@@ -87,7 +87,7 @@
                         circleStrokeOpacity = 0.5,
                         circleStrokeWidth = 1.25,
 
-                        isAxisShow = true,
+                        isAxisShow = false,
 
                         axisFontSize = 12,
                         axisFontWeight = 400,
@@ -99,160 +99,167 @@
                         axisXLaneHeight = isAxisShow ? 30 : 0
                     } = this.options,
                     circleRadius = trackStrokeWidth,
-                    g_w = w - left - right,
-                    g_h = h - top - bottom - axisXLaneHeight,
-                    isAxisDate = isDate(min),
+                    __offsetLeft__ = 20,
+                    __offsetRight__ = 20,
+                    g_w = w - left - right - __offsetLeft__ - __offsetRight__,
+                    g_h = h - top - bottom;
+
+                if (g_h < 2 * circleRadius + circleStrokeWidth + axisXLaneHeight || g_w <= 4 * circleRadius + 2 * circleStrokeWidth) return;
+
+                const isAxisDate = isDate(min),
                     isAxisColor = isString(min),
                     isAxisNumber = isNumber(min),
                     self = this;
 
-                    let hueMin = circleStrokeWidth / 2 + circleRadius,
-                        hueMax = g_w - circleStrokeWidth / 2 - circleRadius,
-                        hueActual = hueMin,
-                        hueTarget = hueMin,
-                        hueTimer = d3.timer(hueTween);
+                let hueMin = circleStrokeWidth / 2 + circleRadius,
+                    hueMax = g_w - circleStrokeWidth / 2 - circleRadius,
+                    hueActual = hueMin,
+                    hueTarget = hueMin,
+                    hueTimer = d3.timer(hueTween);
 
-                    const svg = d3.select(this.$el)
-                        .append('svg')
-                        .attr('width', w)
-                        .attr('height', h);
+                const svg = d3.select(this.$el)
+                    .append('svg')
+                    .attr('width', w)
+                    .attr('height', h);
 
-                    const g = svg
-                        .append('g')
-                        .attr('transform', `translate(${left}, ${top})`);
+                const g = svg
+                    .append('g')
+                    .attr('transform', `translate(${left + __offsetLeft__}, ${top})`)
+                    .attr('width', g_w)
+                    .attr('height', g_h);
 
-                    const track = g
-                        .append('line')
-                        .attr('class', 'track')
-                        .attr('x1', trackStrokeWidth / 2)
-                        .attr('x2', g_w - trackStrokeWidth / 2)
-                        .attr('y1', g_h - circleRadius - circleStrokeWidth / 2)
-                        .attr('y2', g_h - circleRadius - circleStrokeWidth / 2)
-                        .attr('stroke', trackStroke)
-                        .attr('stroke-opacity', trackStrokeOpacity)
-                        .attr('stroke-width', trackStrokeWidth)
-                        .call(roundLine, trackRounded);
+                const track = g
+                    .append('line')
+                    .attr('class', 'track')
+                    .attr('x1', circleRadius + circleStrokeWidth / 2)
+                    .attr('x2', g_w - circleRadius - circleStrokeWidth / 2)
+                    .attr('y1', g_h / 2)
+                    .attr('y2', g_h / 2)
+                    .attr('stroke', trackStroke)
+                    .attr('stroke-opacity', trackStrokeOpacity)
+                    .attr('stroke-width', trackStrokeWidth)
+                    .call(roundLine, trackRounded);
 
-                    const trackInset = g
-                        .append('line')
-                        .attr('class', 'track track--inset')
-                        .attr('x1', trackStrokeWidth / 2)
-                        .attr('x2', g_w - trackStrokeWidth / 2)
-                        .attr('y1', g_h - circleRadius - circleStrokeWidth / 2)
-                        .attr('y2', g_h - circleRadius - circleStrokeWidth / 2)
-                        .attr('stroke', trackInsetStroke)
-                        .attr('stroke-opacity', trackInsetStrokeOpacity)
-                        .attr('stroke-width', trackInsetStrokeWidth)
-                        .call(roundLine, trackRounded);
+                const trackInset = g
+                    .append('line')
+                    .attr('class', 'track track--inset')
+                    .attr('x1', circleRadius + circleStrokeWidth / 2)
+                    .attr('x2', g_w - circleRadius - circleStrokeWidth / 2)
+                    .attr('y1', g_h / 2)
+                    .attr('y2', g_h / 2)
+                    .attr('stroke', trackInsetStroke)
+                    .attr('stroke-opacity', trackInsetStrokeOpacity)
+                    .attr('stroke-width', trackInsetStrokeWidth)
+                    .call(roundLine, trackRounded);
 
-                    const circle = g
-                        .append('circle')
-                        .attr('cx', circleStrokeWidth / 2 + circleRadius)
-                        .attr('cy', g_h - circleRadius - circleStrokeWidth / 2)
-                        .attr('r', circleRadius)
-                        .attr('fill', circleFill)
-                        .attr('stroke', circleStroke)
-                        .attr('stroke-width', circleStrokeWidth)
-                        .attr('stroke-opacity', circleStrokeOpacity);
+                const circle = g
+                    .append('circle')
+                    .attr('cx', circleStrokeWidth / 2 + circleRadius)
+                    .attr('cy', g_h / 2)
+                    .attr('r', circleRadius)
+                    .attr('fill', circleFill)
+                    .attr('stroke', circleStroke)
+                    .attr('stroke-width', circleStrokeWidth)
+                    .attr('stroke-opacity', circleStrokeOpacity);
 
-                    const xScale = (function(isAxisDate, isAxisColor, isAxisNumber) {
-                        if (isAxisNumber) {
-                            return d3.scaleLinear()
-                                .domain([min, max])
-                                .range([circleRadius + circleStrokeWidth / 2, g_w - circleStrokeWidth  - 2 * circleRadius])
-                                .clamp(true);
-                        }
+                const xScale = (function(isAxisDate, isAxisColor, isAxisNumber) {
+                    if (isAxisNumber) {
+                        return d3.scaleLinear()
+                            .domain([min, max])
+                            .range([0, g_w - circleStrokeWidth - 2 * circleRadius])
+                            .clamp(true);
+                    }
 
-                        if (isAxisDate) {
-                            return d3.scaleTime()
-                                .domain([min, max])
-                                .range([circleRadius + circleStrokeWidth / 2, g_w - circleStrokeWidth  - 2 * circleRadius])
-                                .clamp(true);
-                        }
+                    if (isAxisDate) {
+                        return d3.scaleTime()
+                            .domain([min, max])
+                            .range([0, g_w - circleStrokeWidth - 2 * circleRadius])
+                            .clamp(true);
+                    }
 
-                        if (isAxisColor) {
-                            return d3.scalePoint()
-                                .domain([min, max])
-                                .range([circleRadius + circleStrokeWidth / 2, g_w - circleStrokeWidth  - 2 * circleRadius]);
-                        }
-                    })(isAxisDate, isAxisColor, isAxisNumber);
+                    if (isAxisColor) {
+                        return d3.scalePoint()
+                            .domain([min, max])
+                            .range([0, g_w - circleStrokeWidth - 2 * circleRadius]);
+                    }
+                })(isAxisDate, isAxisColor, isAxisNumber);
 
+                if (isAxisShow) {
                     const xAxis = d3
                         .axisBottom(xScale);
 
                     const axisXLane = svg
                         .append('g')
-                        .attr('transform',  `translate(${left + circleStrokeWidth / 2 + circleRadius}, ${top + g_h})`)
+                        .attr('transform',  `translate(${left + __offsetLeft__ + circleStrokeWidth / 2 + circleRadius}, ${top + g_h / 2 + circleRadius + circleStrokeWidth / 2})`)
                         .attr('class', 'axis axis--x')
                         .attr('width', g_w - circleStrokeWidth - 2 * circleRadius)
                         .attr('height', axisXLaneHeight);
 
                     axisXLane
                         .call(xAxis)
-                        .call(firstTickTextAnchorStart)
-                        .call(lastTickTextAnchorEnd)
                         .call(responsiveAxisX, xAxis, xScale)
                         .call(axisShow, false, false)
                         .attr('font-size', axisFontSize)
                         .attr('opacity', axisFontOpacity)
                         .attr('font-weight', axisFontWeight);
+                }
 
-                    function hueTween(x) {
-                        const hueError = hueTarget - hueActual;
+                function hueTween(x) {
+                    const hueError = hueTarget - hueActual;
 
-                        let val = isAxisColor
-                                ? d3.interpolate(min, max)((hueActual - hueMin) / (hueMax - hueMin))
-                                : xScale.invert(hueActual);
+                    let val = isAxisColor
+                            ? d3.interpolate(min, max)((hueActual - hueMin) / (hueMax - hueMin))
+                            : xScale.invert(hueActual - circleRadius - circleStrokeWidth / 2);
 
-                        if (Math.abs(hueError) < 1e-3) {
-                            hueActual = hueTarget, hueTimer.stop(), hideTip();
+                    if (Math.abs(hueError) < 1e-3) {
+                        hueActual = hueTarget, hueTimer.stop(), hideTip();
 
-                            emit(self, 'dragend', val);
-                        }
+                        emit(self, 'dragend', val);
+                    }
 
-                        else {
-                            hueActual += hueAlpha * hueError;
+                    else {
+                        hueActual += hueAlpha * hueError;
 
-                            circle
-                                .attr('cx', hueActual);
+                        circle
+                            .attr('cx', hueActual);
 
-                                showTip(typeof val === 'number'? val.toFixed(2) : val, circle.node())();
+                            showTip(typeof val === 'number'? val.toFixed(2) : val, circle.node())();
 
-                            emit(self, 'dragging', val);
-                        }
-                    };
+                        emit(self, 'dragging', val);
+                    }
+                };
 
-                    function hue(x) {
-                        if (x >= circleRadius + circleStrokeWidth / 2 && x <= g_w - circleStrokeWidth / 2 - circleRadius)
-                        hueTarget = x, hueTimer.restart(hueTween);
-                    };
+                function hue(x) {
+                    if (x >= circleRadius + circleStrokeWidth / 2 && x <= g_w - circleStrokeWidth / 2 - circleRadius)
+                    hueTarget = x, hueTimer.restart(hueTween);
+                };
 
-                    const trackOverlay = g
-                        .append('rect')
-                        .attr('x', 0)
-                        .attr('y', g_h - circleRadius * 2 - circleStrokeWidth)
-                        .attr('width', g_w)
-                        .attr('height', circleRadius * 2 + circleStrokeWidth)
-                        .attr('fill', 'none')
-                        .attr('pointer-events', 'all')
-                        .attr('cursor', 'pointer')
-                        .call(d3.drag()
-                            .on('start.interrupt', function() {
-                                trackOverlay.interrupt();
-                            })
-                            .on('start drag', function() {
-                                hue(d3.event.x);
-                        }));
-
-                    trackOverlay
-                        .on('mouseover', function() {
-                            let val = isAxisColor
-                                ? d3.interpolate(min, max)((hueActual - hueMin) / (hueMax - hueMin))
-                                : xScale.invert(hueActual);
-
-                            showTip(typeof val === 'number' ? val.toFixed(2) : val, circle.node())();
+                const trackOverlay = g
+                    .append('rect')
+                    .attr('x', 0)
+                    .attr('y', g_h / 2 - circleRadius - circleStrokeWidth / 2)
+                    .attr('width', g_w)
+                    .attr('height', circleRadius * 2 + circleStrokeWidth)
+                    .attr('fill', 'none')
+                    .attr('pointer-events', 'all')
+                    .attr('cursor', 'pointer')
+                    .call(d3.drag()
+                        .on('start.interrupt', function() {
+                            trackOverlay.interrupt();
                         })
-                        .on('mouseout', hideTip);
+                        .on('start drag', function() {
+                            hue(d3.event.x);
+                    }));
+
+                trackOverlay
+                    .on('mouseover', function() {
+                        let val = isAxisColor
+                            ? d3.interpolate(min, max)((hueActual - hueMin) / (hueMax - hueMin))
+                            : xScale.invert(hueActual - circleRadius - circleStrokeWidth / 2);
+
+                        showTip(typeof val === 'number' ? val.toFixed(2) : val, circle.node())();
+                    })
+                    .on('mouseout', hideTip);
             },
             safeDraw() {
                 this.ifExistsSvgThenRemove();
