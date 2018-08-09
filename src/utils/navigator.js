6dfcc53f4f1c861e810015591ed3a1b88e074884
Cheng JIANG <cheng.jiang@outlook.fr>
2018-08-09 16:19:45 +0200

    [ADD]: hardwareConcurrency in utils/navigator.js

--- src/utils/navigator.js
+++ src/utils/navigator.js
@@ -1,1 +1,12 @@
+/**
+ *
+ * detect if the navigator is firefox
+ *
+ * @type {boolean}
+ */
 export const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
+/**
+ * max concurrency of navigator
+ */
+export const hardwareConcurrency = navigator.hardwareConcurrency || 4;
