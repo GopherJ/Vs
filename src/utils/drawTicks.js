983f1083d1a9757903c41e03f02677c4d7cebed1
Cheng JIANG <cheng.jiang@outlook.fr>
2018-08-10 23:18:49 +0200

    [ADD]: clipPathId

--- src/utils/drawTicks.js
+++ src/utils/drawTicks.js
@@ -3,19 +3,18 @@
  * draw continious scale's ticks for axis x
  *
  * @param {d3.Selection} g
- * @param {d3.ScaleTime} scale
+ * @param {d3.ScaleTime | d3.ScaleLinear} scale
  * @param {number} h
+ * @param {string} clipPathId
  * @param {string} stroke
  * @param {number} strokeWidth
- * @param {string} clipPath
  * @return {void}
  */
-function drawTicksX(g, scale, h, stroke, strokeWidth) {
+function drawTicksX(g, scale, h, clipPathId, stroke, strokeWidth) {
     const ticks = scale.ticks(),
         ticksSelection = g.selectAll('.line--tick');
 
-    if (!ticksSelection.empty())
-        ticksSelection.remove();
+    if (!ticksSelection.empty()) ticksSelection.remove();
 
     g.selectAll('.line--tick')
         .data(ticks)
@@ -25,6 +24,7 @@ function drawTicksX(g, scale, h, stroke, strokeWidth) {
         .attr('x1', d => scale(d))
         .attr('x2', d => scale(d))
         .attr('y2', h)
+        .attr('clip-path', `url(#${clipPathId})`)
         .attr('stroke', stroke)
         .attr('stroke-width', strokeWidth)
         .attr('pointer-events', 'none');
@@ -38,16 +38,16 @@ function drawTicksX(g, scale, h, stroke, strokeWidth) {
  * @param {d3.Selection} g
  * @param {d3.ScaleTime} scale
  * @param {number} w
+ * @param {string} clipPathId
  * @param {string} stroke
  * @param {number} strokeWidth
  * @return {void}
  */
-function drawTicksY(g, scale, w, stroke, strokeWidth) {
+function drawTicksY(g, scale, w, clipPathId, stroke, strokeWidth) {
     const ticks = scale.ticks(),
         ticksSelection = g.selectAll('.line--tick');
 
-    if (!ticksSelection.empty())
-        ticksSelection.remove();
+    if (!ticksSelection.empty()) ticksSelection.remove();
 
     g.selectAll('.line--tick')
         .data(ticks)
@@ -57,13 +57,11 @@ function drawTicksY(g, scale, w, stroke, strokeWidth) {
         .attr('y1', d => scale(d))
         .attr('y2', d => scale(d))
         .attr('x2', w)
+        .attr('clip-path', `url(#${clipPathId})`)
         .attr('stroke', stroke)
         .attr('stroke-width', strokeWidth)
         .attr('pointer-events', 'none');
 }
-
-
-
 
 export {
     drawTicksX,
