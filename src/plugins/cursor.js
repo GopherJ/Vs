6367d580810ac58734693eb3cb809f5b89533d16
Cheng JIANG <cheng.jiang@outlook.fr>
2018-08-15 22:32:44 +0200

    [IMPROVE]: code structure

--- src/plugins/cursor.js
+++ src/plugins/cursor.js
@@ -1,27 +1,22 @@
 import * as d3 from 'd3';
+
 /**
  *
  * cursor control in svg
  *
- * @param {d3.Selection} svg
- * @param {d3.Selection} g
- * @param {[[number, number], [number, number]]} extent
- * @param {string} cursor
+ * @param svg
+ * @param g
+ * @param extent
+ * @param cursor
  */
 function cursor(svg, g, extent, cursor = 'pointer') {
-    let coordinates = [0, 0], [[x0, y0], [x1, y1]] = extent;
+    const [[x0, y0], [x1, y1]] = extent;
 
     svg.on('mousemove', function () {
-        coordinates = d3.mouse(g.node());
-        const [cx, cy] = coordinates;
+        const [cx, cy] = d3.mouse(g.node());
 
-        if (cx > x0 && cx < x1 && cy > y0 && cy < y1) {
-            svg.attr('cursor', cursor);
-        }
-
-        else {
-            svg.attr('cursor', 'auto');
-        }
+        if (cx > x0 && cx < x1 && cy > y0 && cy < y1) svg.attr('cursor', cursor);
+        else svg.attr('cursor', 'auto');
     });
 }
 
