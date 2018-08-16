2bb98eb3dffb5cd8f06bfe30850de2473161247e
Cheng JIANG <cheng.jiang@outlook.fr>
2018-08-16 11:12:40 +0200

    [IMPROVE]: dont remove it when it exists

--- src/plugins/drawCurrentReference.js
+++ src/plugins/drawCurrentReference.js
@@ -8,22 +8,25 @@
  * @param currentTimeLineWidth
  */
 const drawCurrentReferenceX = (g, xScale, g_h, clipPathId, currentTimeLineColor, currentTimeLineWidth) => {
-    const referenceSelection = g.select('.line--reference');
-    if (!referenceSelection.empty()) referenceSelection.remove();
+    let date = new Date(),
+        referenceSelection = g.select('.line--reference');
 
-    const date = new Date();
-
-    g
-        .append('line')
-        .attr('class', 'line--reference')
-        .attr('x1', xScale(date))
-        .attr('x2', xScale(date))
-        .attr('y1', 0)
-        .attr('y2', g_h)
-        .attr('stroke', currentTimeLineColor)
-        .attr('stroke-width', currentTimeLineWidth)
-        .attr('clip-path', `url(#${clipPathId})`)
-        .attr('pointer-events', 'none');
+    if (!referenceSelection.empty()) {
+        referenceSelection
+            .attr('x1', xScale(date))
+            .attr('x2', xScale(date));
+    } else {
+        referenceSelection = g.append('line')
+            .attr('class', 'line--reference')
+            .attr('x1', xScale(date))
+            .attr('x2', xScale(date))
+            .attr('y1', 0)
+            .attr('y2', g_h)
+            .attr('stroke', currentTimeLineColor)
+            .attr('stroke-width', currentTimeLineWidth)
+            .attr('clip-path', `url(#${clipPathId})`)
+            .attr('pointer-events', 'none');
+    }
 };
 
 export {
