59787aa5d1b4befc4069ffad8c169ca190b45ab6
Cheng JIANG <cheng.jiang@outlook.fr>
2018-08-19 15:52:42 +0200

    [ADD]: be able to disable zoom on some area

---
+++ src/plugins/disableZoomOn.js
@@ -0,0 +1,17 @@
+import * as d3 from 'd3';
+
+/**
+ *
+ * @param svg
+ * @param extent
+ */
+const disableZoomOn = (svg, extent) => {
+    svg.on('mousedown', () => {
+        const [cx, cy] = d3.mouse(svg.node()),
+            [[x0, y0], [x1, y1]] = extent;
+
+        if (cx < x0 || cx > x1 || cy > y1 || cy < y0) d3.event.stopImmediatePropagation();
+    });
+};
+
+export default disableZoomOn;
--- src/plugins/zoom.js
+++ src/plugins/zoom.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
 import { isFunction } from 'lodash';
 import { isArray } from '../utils/array';
+import disableZoomOn from './disableZoomOn';
 
 /**
  *
@@ -9,17 +10,20 @@ import { isArray } from '../utils/array';
  * @param zoomend
  * @param zoomstart
  * @param scaleExtent
+ * @param extent
  */
-function zoom(svg, { zooming, zoomend, zoomstart }, scaleExtent) {
+function zoom(svg, { zooming, zoomend, zoomstart }, scaleExtent, extent) {
     const zoom = d3.zoom();
 
     if (isArray(scaleExtent)) zoom.scaleExtent(scaleExtent);
+    if (isArray(extent)) svg.call(disableZoomOn, extent);
 
     if (isFunction(zooming))   zoom.on('zoom', zooming);
     if (isFunction(zoomend))   zoom.on('end', zoomend);
     if (isFunction(zoomstart)) zoom.on('start', zoomstart);
 
     svg.call(zoom);
+    svg.on('dblclick.zoom', null);
 }
 
 export default zoom;
