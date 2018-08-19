2c0b7bdb74fd96d7bc8a932fb098a1c3d1472ac5
Cheng JIANG <cheng.jiang@outlook.fr>
2018-08-19 16:40:03 +0200

    [ADD]: isExtent && isSimpleExtent check

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,5 +1,9 @@
 export const isArray = Array.isArray;
 
+export const isSimpleExtent = arr => isArray(arr) && arr.length === 2;
+
+export const isExtent = arr => isArray(arr) && isArray(arr[0]) && arr[0].length === 2 && isArray(arr[1]) && arr[1].length === 2;
+
 export const map = Array.prototype.map;
 
 export const slice = Array.prototype.slice;
