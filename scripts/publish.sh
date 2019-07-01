54af197051750cae3201e1ecc6bd33d158eda259
Cheng JIANG <cheng.jiang@outlook.fr>
2019-07-01 12:15:31 +0800

    [dump]: v0.9.5

---
+++ scripts/publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$( git rev-parse --show-toplevel )"
+
+npm run build \
+    && sed -i "/version/c\    \"version\":\"$1\"," "$ROOT/package.json" \
+    && git add $ROOT \
+    && git commit -m "[dump]: v$1" \
+    && git tag v"$1" \
+    && git push origin master \
+    && git push origin v"$1" \
+    && npm publish $ROOT \
