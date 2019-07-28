a51bddd49a1dc9ee970ec122f7e30e61f6a2593f
Cheng JIANG <cheng.jiang@outlook.fr>
2019-07-28 14:25:26 +0800

    [ignore]: firefox error

--- src/plugins/moveReference.js
+++ src/plugins/moveReference.js
@@ -5,14 +5,18 @@
  * @param reference
  */
 const moveReferenceX = (g, xScale, reference) => {
-    const line = g.select('.line--reference'),
-        overlay = g.select('.overlay'),
-        overlayWidth = +overlay.node().getBBox().width,
-        x = xScale(reference);
+    try {
+        const line        = g.select('.line--reference'),
+            overlay       = g.select('.overlay'),
+            overlayWidth  = +overlay.node().getBBox().width,
+            x             = xScale(reference);
 
-    overlay.attr('x', x - overlayWidth / 2);
+        overlay.attr('x', x - overlayWidth / 2);
 
-    line.attr('x1', x).attr('x2', x);
+        line.attr('x1', x).attr('x2', x);
+    } catch(e) {
+        console.log(e.message);
+    }
 };
 
 export {
